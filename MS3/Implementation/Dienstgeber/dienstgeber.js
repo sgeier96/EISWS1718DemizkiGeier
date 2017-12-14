0f398d490f902de505b026e015ed2b2500e15c87
sgeier96 <Geier_Stefan@gmx.de>
2017-12-14 19:51:10 +0100

    further testing of json parsing

--- MS3/Implementation/Dienstgeber/dienstgeber.js
+++ MS3/Implementation/Dienstgeber/dienstgeber.js
@@ -36,7 +36,7 @@ app.get('/*', function(request, response){
 
 app.post('/', function(request, response){
   console.log(request.body);  //our JSON
-  response.send(request.residents); //echoing the result back
+  response.send(request.availableResources[0].name); //echoing the result back
 });
 
 
