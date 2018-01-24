e2aefbe2e1c60888369019c7864e7556d06d77f9
vdemizki <vadim.demizki@gmx.de>
2018-01-24 12:39:47 +0100

    added api communication and xml validate function
    -added also xmlhttprequest package
    -added also xmllint package

--- MS3/Implementation/Dienstgeber/dienstgeber.js
+++ MS3/Implementation/Dienstgeber/dienstgeber.js
@@ -4,6 +4,8 @@ var request = require("request");         //request(url, function(err, res, body
 var bodyParser = require("body-parser");  //parser for i.e. JSON
 
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;//For sync/async Requests
+var DomParser = require('dom-parser');
+var xmllint = require('xmllint')
 
 var app = express();
 var households = [];
@@ -32,10 +34,70 @@ app.post('/', function(request, response){
   console.log("[POST-Acceptor] Received data: " + JSON.stringify(request.body));
   var data = request.body;
   processRequestParameter(data);
+  vrsCommunication();
 
   response.send(result);
 });
 
+//-----------------------------------------TEST API Anbindung----------------------------------//
+function vrsCommunication(){
+  var xhr = new XMLHttpRequest(); //creates an XMLHttpRequest object
+//  var xmlParse = require('xml-parser');
+//  var inspect = require('util').inspect;
+  var oParser = new DomParser();
+
+  var xmlPOST =
+  '<?xml version="1.0" encoding="ISO-8859-15"?>'            +
+  ' <Request>'                                              +
+  '   <ObjectInfo>'                                         +
+  '     <ObjectSearch>'                                     +
+  '       <String>rathaus köln</String>'                    +
+  '       <Classes>'                                        +
+  '         <Stop/>'                                        +
+  '       </Classes>'                                       +
+  '     </ObjectSearch>'                                    +
+  '      <Options>'                                         +
+  '        <Output>'                                        +
+  '         <SRSName>urn:adv:crs:ETRS89_UTM32</SRSName>'    +
+  '        </Output>'                                       +
+  '      </Options>'                                        +
+  '   </ObjectInfo>'                                        +
+  '</Request>';
+  //validate(xmlPOST);
+
+  xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        //var oDOM = oParser.parseFromString(xhr.responseText, "text/xml");
+        var strResponse = xhr.responseText;
+        //validate(strResponse);
+      }
+      else {
+        console.log("failed request");
+      }
+  }
+  xhr.open("POST","https://apitest.vrsinfo.de:4443/vrs/cgi/service/ass",false); //Specifies the type(method,url,async) of request
+  xhr.setRequestHeader("POST", "https://apitest.vrsinfo.de:4443/vrs/cgi/service/ass"); //Set Request Header
+  xhr.setRequestHeader("Content-Type", "text/xml");
+  xhr.setRequestHeader("Charset", "ISO-8859-15");
+  xhr.send(xmlPOST);        //Sends a request string to the server
+}
+
+function  validate (xml){
+  var schema = fs.readFileSync('./ASS2Schnittstelle_3.0.4.xsd').toString();
+    //var xml = fs.readFileSync('./xmlPost.xml').toString();
+    /*xmllint.validateXML({ //https://www.npmjs.com/package/xmllint
+    xml: xml,
+    schema: schema});
+    */
+   try {
+     xmllint.validateXML({ xml: xml, schema: schema});
+     console.log("validate success");
+   }
+   catch(error) {
+     console.log("validate failed");
+   }
+}
+//---------------------------------------------------------------------------------------------//
 
 
 function processRequestParameter(data){
